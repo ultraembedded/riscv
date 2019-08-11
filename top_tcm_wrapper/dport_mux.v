6d5dda3de62d28606ea222eca61b14cc7417b97f
ultraembedded <ultraembedded.com@gmail.com>
2019-08-11 22:15:20 +0100

    Add another wrapper level for the RISC-V core

---
+++ top_tcm_wrapper/dport_mux.v
@@ -0,0 +1,158 @@
+//-----------------------------------------------------------------
+//                         RISC-V Top
+//                            V0.5
+//                     Ultra-Embedded.com
+//                     Copyright 2014-2019
+//
+//                   admin@ultra-embedded.com
+//
+//                       License: BSD
+//-----------------------------------------------------------------
+//
+// Copyright (c) 2014, Ultra-Embedded.com
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions 
+// are met:
+//   - Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//   - Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer 
+//     in the documentation and/or other materials provided with the 
+//     distribution.
+//   - Neither the name of the author nor the names of its contributors 
+//     may be used to endorse or promote products derived from this 
+//     software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+// SUCH DAMAGE.
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+//                          Generated File
+//-----------------------------------------------------------------
+module dport_mux
+(
+    // Inputs
+     input           clk_i
+    ,input           rst_i
+    ,input  [ 31:0]  mem_addr_i
+    ,input  [ 31:0]  mem_data_wr_i
+    ,input           mem_rd_i
+    ,input  [  3:0]  mem_wr_i
+    ,input           mem_cacheable_i
+    ,input  [ 10:0]  mem_req_tag_i
+    ,input           mem_invalidate_i
+    ,input           mem_flush_i
+    ,input  [ 31:0]  mem_tcm_data_rd_i
+    ,input           mem_tcm_accept_i
+    ,input           mem_tcm_ack_i
+    ,input           mem_tcm_error_i
+    ,input  [ 10:0]  mem_tcm_resp_tag_i
+    ,input  [ 31:0]  mem_ext_data_rd_i
+    ,input           mem_ext_accept_i
+    ,input           mem_ext_ack_i
+    ,input           mem_ext_error_i
+    ,input  [ 10:0]  mem_ext_resp_tag_i
+
+    // Outputs
+    ,output [ 31:0]  mem_data_rd_o
+    ,output          mem_accept_o
+    ,output          mem_ack_o
+    ,output          mem_error_o
+    ,output [ 10:0]  mem_resp_tag_o
+    ,output [ 31:0]  mem_tcm_addr_o
+    ,output [ 31:0]  mem_tcm_data_wr_o
+    ,output          mem_tcm_rd_o
+    ,output [  3:0]  mem_tcm_wr_o
+    ,output          mem_tcm_cacheable_o
+    ,output [ 10:0]  mem_tcm_req_tag_o
+    ,output          mem_tcm_invalidate_o
+    ,output          mem_tcm_flush_o
+    ,output [ 31:0]  mem_ext_addr_o
+    ,output [ 31:0]  mem_ext_data_wr_o
+    ,output          mem_ext_rd_o
+    ,output [  3:0]  mem_ext_wr_o
+    ,output          mem_ext_cacheable_o
+    ,output [ 10:0]  mem_ext_req_tag_o
+    ,output          mem_ext_invalidate_o
+    ,output          mem_ext_flush_o
+);
+
+
+
+//-----------------------------------------------------------------
+// Dcache_if mux
+//-----------------------------------------------------------------
+wire hold_w;
+
+/* verilator lint_off UNSIGNED */
+wire tcm_access_w = (mem_addr_i >= 32'h0 && mem_addr_i < (32'h0 + 32'd65536));
+/* verilator lint_on UNSIGNED */
+
+reg       tcm_access_q;
+reg [4:0] pending_q;
+
+assign mem_tcm_addr_o       = mem_addr_i;
+assign mem_tcm_data_wr_o    = mem_data_wr_i;
+assign mem_tcm_rd_o         = (tcm_access_w & ~hold_w) ? mem_rd_i : 1'b0;
+assign mem_tcm_wr_o         = (tcm_access_w & ~hold_w) ? mem_wr_i : 4'b0;
+assign mem_tcm_cacheable_o  = mem_cacheable_i;
+assign mem_tcm_req_tag_o    = mem_req_tag_i;
+assign mem_tcm_invalidate_o = (tcm_access_w & ~hold_w) ? mem_invalidate_i : 1'b0;
+assign mem_tcm_flush_o      = (tcm_access_w & ~hold_w) ? mem_flush_i : 1'b0;
+
+assign mem_ext_addr_o       = mem_addr_i;
+assign mem_ext_data_wr_o    = mem_data_wr_i;
+assign mem_ext_rd_o         = (~tcm_access_w & ~hold_w) ? mem_rd_i : 1'b0;
+assign mem_ext_wr_o         = (~tcm_access_w & ~hold_w) ? mem_wr_i : 4'b0;
+assign mem_ext_cacheable_o  = mem_cacheable_i;
+assign mem_ext_req_tag_o    = mem_req_tag_i;
+assign mem_ext_invalidate_o = (~tcm_access_w & ~hold_w) ? mem_invalidate_i : 1'b0;
+assign mem_ext_flush_o      = (~tcm_access_w & ~hold_w) ? mem_flush_i : 1'b0;
+
+assign mem_accept_o         =(tcm_access_w ? mem_tcm_accept_i   : mem_ext_accept_i) & !hold_w;
+assign mem_data_rd_o        = tcm_access_q ? mem_tcm_data_rd_i  : mem_ext_data_rd_i;
+assign mem_ack_o            = tcm_access_q ? mem_tcm_ack_i      : mem_ext_ack_i;
+assign mem_error_o          = tcm_access_q ? mem_tcm_error_i    : mem_ext_error_i;
+assign mem_resp_tag_o       = tcm_access_q ? mem_tcm_resp_tag_i : mem_ext_resp_tag_i;
+
+reg [4:0] pending_r;
+always @ *
+begin
+    pending_r = pending_q;
+
+    if (((mem_rd_i || mem_wr_i != 4'b0) && mem_accept_o) && !mem_ack_o)
+        pending_r = pending_r + 5'd1;
+    else if (!((mem_rd_i || mem_wr_i != 4'b0) && mem_accept_o) && mem_ack_o)
+        pending_r = pending_r - 5'd1;
+end
+
+always @ (posedge clk_i or posedge rst_i)
+if (rst_i)
+    pending_q <= 5'b0;
+else
+    pending_q <= pending_r;
+
+always @ (posedge clk_i or posedge rst_i)
+if (rst_i)
+    tcm_access_q <= 1'b0;
+else if ((mem_rd_i || mem_wr_i != 4'b0) && mem_accept_o)
+    tcm_access_q <= tcm_access_w;
+
+assign hold_w = (|pending_q) && (tcm_access_q != tcm_access_w);
+
+
+
+endmodule
---
+++ top_tcm_wrapper/riscv_tcm_wrapper.v
@@ -0,0 +1,367 @@
+//-----------------------------------------------------------------
+//                         RISC-V Top
+//                            V0.5
+//                     Ultra-Embedded.com
+//                     Copyright 2014-2019
+//
+//                   admin@ultra-embedded.com
+//
+//                       License: BSD
+//-----------------------------------------------------------------
+//
+// Copyright (c) 2014, Ultra-Embedded.com
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions 
+// are met:
+//   - Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//   - Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer 
+//     in the documentation and/or other materials provided with the 
+//     distribution.
+//   - Neither the name of the author nor the names of its contributors 
+//     may be used to endorse or promote products derived from this 
+//     software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+// SUCH DAMAGE.
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+//                          Generated File
+//-----------------------------------------------------------------
+module riscv_tcm_wrapper
+//-----------------------------------------------------------------
+// Params
+//-----------------------------------------------------------------
+#(
+     parameter BOOT_VECTOR      = 0
+    ,parameter CORE_ID          = 0
+)
+//-----------------------------------------------------------------
+// Ports
+//-----------------------------------------------------------------
+(
+    // Inputs
+     input           clk_i
+    ,input           rst_i
+    ,input           rst_cpu_i
+    ,input           axi_i_awready_i
+    ,input           axi_i_wready_i
+    ,input           axi_i_bvalid_i
+    ,input  [  1:0]  axi_i_bresp_i
+    ,input  [  3:0]  axi_i_bid_i
+    ,input           axi_i_arready_i
+    ,input           axi_i_rvalid_i
+    ,input  [ 31:0]  axi_i_rdata_i
+    ,input  [  1:0]  axi_i_rresp_i
+    ,input  [  3:0]  axi_i_rid_i
+    ,input           axi_i_rlast_i
+    ,input           axi_t_awvalid_i
+    ,input  [ 31:0]  axi_t_awaddr_i
+    ,input  [  3:0]  axi_t_awid_i
+    ,input  [  7:0]  axi_t_awlen_i
+    ,input  [  1:0]  axi_t_awburst_i
+    ,input           axi_t_wvalid_i
+    ,input  [ 31:0]  axi_t_wdata_i
+    ,input  [  3:0]  axi_t_wstrb_i
+    ,input           axi_t_wlast_i
+    ,input           axi_t_bready_i
+    ,input           axi_t_arvalid_i
+    ,input  [ 31:0]  axi_t_araddr_i
+    ,input  [  3:0]  axi_t_arid_i
+    ,input  [  7:0]  axi_t_arlen_i
+    ,input  [  1:0]  axi_t_arburst_i
+    ,input           axi_t_rready_i
+    ,input  [ 31:0]  intr_i
+
+    // Outputs
+    ,output          axi_i_awvalid_o
+    ,output [ 31:0]  axi_i_awaddr_o
+    ,output [  3:0]  axi_i_awid_o
+    ,output [  7:0]  axi_i_awlen_o
+    ,output [  1:0]  axi_i_awburst_o
+    ,output          axi_i_wvalid_o
+    ,output [ 31:0]  axi_i_wdata_o
+    ,output [  3:0]  axi_i_wstrb_o
+    ,output          axi_i_wlast_o
+    ,output          axi_i_bready_o
+    ,output          axi_i_arvalid_o
+    ,output [ 31:0]  axi_i_araddr_o
+    ,output [  3:0]  axi_i_arid_o
+    ,output [  7:0]  axi_i_arlen_o
+    ,output [  1:0]  axi_i_arburst_o
+    ,output          axi_i_rready_o
+    ,output          axi_t_awready_o
+    ,output          axi_t_wready_o
+    ,output          axi_t_bvalid_o
+    ,output [  1:0]  axi_t_bresp_o
+    ,output [  3:0]  axi_t_bid_o
+    ,output          axi_t_arready_o
+    ,output          axi_t_rvalid_o
+    ,output [ 31:0]  axi_t_rdata_o
+    ,output [  1:0]  axi_t_rresp_o
+    ,output [  3:0]  axi_t_rid_o
+    ,output          axi_t_rlast_o
+);
+
+wire  [ 31:0]  ifetch_pc_w;
+wire  [ 31:0]  dport_tcm_data_rd_w;
+wire           dport_tcm_cacheable_w;
+wire           dport_flush_w;
+wire  [  3:0]  dport_tcm_wr_w;
+wire           ifetch_rd_w;
+wire           dport_axi_accept_w;
+wire           dport_cacheable_w;
+wire           dport_tcm_flush_w;
+wire  [ 10:0]  dport_resp_tag_w;
+wire  [ 10:0]  dport_axi_resp_tag_w;
+wire           ifetch_accept_w;
+wire  [ 31:0]  dport_data_rd_w;
+wire           dport_tcm_invalidate_w;
+wire           dport_ack_w;
+wire  [ 10:0]  dport_axi_req_tag_w;
+wire  [ 31:0]  dport_data_wr_w;
+wire           dport_invalidate_w;
+wire  [ 10:0]  dport_tcm_req_tag_w;
+wire  [ 31:0]  dport_tcm_addr_w;
+wire           dport_axi_error_w;
+wire           dport_tcm_ack_w;
+wire           dport_tcm_rd_w;
+wire  [ 10:0]  dport_tcm_resp_tag_w;
+wire  [ 31:0]  cpu_id_w = CORE_ID;
+wire           dport_rd_w;
+wire           dport_axi_ack_w;
+wire           dport_axi_rd_w;
+wire  [ 31:0]  dport_axi_data_rd_w;
+wire           dport_axi_invalidate_w;
+wire  [ 31:0]  boot_vector_w = BOOT_VECTOR;
+wire  [ 31:0]  dport_addr_w;
+wire           ifetch_error_w;
+wire  [ 31:0]  dport_tcm_data_wr_w;
+wire           ifetch_flush_w;
+wire  [ 31:0]  dport_axi_addr_w;
+wire           dport_error_w;
+wire           dport_tcm_accept_w;
+wire           ifetch_invalidate_w;
+wire  [  3:0]  dport_wr_w;
+wire           ifetch_valid_w;
+wire  [ 31:0]  dport_axi_data_wr_w;
+wire  [ 10:0]  dport_req_tag_w;
+wire  [ 31:0]  ifetch_inst_w;
+wire           dport_axi_cacheable_w;
+wire  [  3:0]  dport_axi_wr_w;
+wire           dport_axi_flush_w;
+wire           dport_tcm_error_w;
+wire           dport_accept_w;
+
+
+riscv_core
+u_core
+(
+    // Inputs
+     .clk_i(clk_i)
+    ,.rst_i(rst_cpu_i)
+    ,.mem_d_data_rd_i(dport_data_rd_w)
+    ,.mem_d_accept_i(dport_accept_w)
+    ,.mem_d_ack_i(dport_ack_w)
+    ,.mem_d_error_i(dport_error_w)
+    ,.mem_d_resp_tag_i(dport_resp_tag_w)
+    ,.mem_i_accept_i(ifetch_accept_w)
+    ,.mem_i_valid_i(ifetch_valid_w)
+    ,.mem_i_error_i(ifetch_error_w)
+    ,.mem_i_inst_i(ifetch_inst_w)
+    ,.intr_i(intr_i[0:0])
+    ,.reset_vector_i(boot_vector_w)
+    ,.cpu_id_i(cpu_id_w)
+
+    // Outputs
+    ,.mem_d_addr_o(dport_addr_w)
+    ,.mem_d_data_wr_o(dport_data_wr_w)
+    ,.mem_d_rd_o(dport_rd_w)
+    ,.mem_d_wr_o(dport_wr_w)
+    ,.mem_d_cacheable_o(dport_cacheable_w)
+    ,.mem_d_req_tag_o(dport_req_tag_w)
+    ,.mem_d_invalidate_o(dport_invalidate_w)
+    ,.mem_d_flush_o(dport_flush_w)
+    ,.mem_i_rd_o(ifetch_rd_w)
+    ,.mem_i_flush_o(ifetch_flush_w)
+    ,.mem_i_invalidate_o(ifetch_invalidate_w)
+    ,.mem_i_pc_o(ifetch_pc_w)
+);
+
+
+dport_mux
+u_dmux
+(
+    // Inputs
+     .clk_i(clk_i)
+    ,.rst_i(rst_i)
+    ,.mem_addr_i(dport_addr_w)
+    ,.mem_data_wr_i(dport_data_wr_w)
+    ,.mem_rd_i(dport_rd_w)
+    ,.mem_wr_i(dport_wr_w)
+    ,.mem_cacheable_i(dport_cacheable_w)
+    ,.mem_req_tag_i(dport_req_tag_w)
+    ,.mem_invalidate_i(dport_invalidate_w)
+    ,.mem_flush_i(dport_flush_w)
+    ,.mem_tcm_data_rd_i(dport_tcm_data_rd_w)
+    ,.mem_tcm_accept_i(dport_tcm_accept_w)
+    ,.mem_tcm_ack_i(dport_tcm_ack_w)
+    ,.mem_tcm_error_i(dport_tcm_error_w)
+    ,.mem_tcm_resp_tag_i(dport_tcm_resp_tag_w)
+    ,.mem_ext_data_rd_i(dport_axi_data_rd_w)
+    ,.mem_ext_accept_i(dport_axi_accept_w)
+    ,.mem_ext_ack_i(dport_axi_ack_w)
+    ,.mem_ext_error_i(dport_axi_error_w)
+    ,.mem_ext_resp_tag_i(dport_axi_resp_tag_w)
+
+    // Outputs
+    ,.mem_data_rd_o(dport_data_rd_w)
+    ,.mem_accept_o(dport_accept_w)
+    ,.mem_ack_o(dport_ack_w)
+    ,.mem_error_o(dport_error_w)
+    ,.mem_resp_tag_o(dport_resp_tag_w)
+    ,.mem_tcm_addr_o(dport_tcm_addr_w)
+    ,.mem_tcm_data_wr_o(dport_tcm_data_wr_w)
+    ,.mem_tcm_rd_o(dport_tcm_rd_w)
+    ,.mem_tcm_wr_o(dport_tcm_wr_w)
+    ,.mem_tcm_cacheable_o(dport_tcm_cacheable_w)
+    ,.mem_tcm_req_tag_o(dport_tcm_req_tag_w)
+    ,.mem_tcm_invalidate_o(dport_tcm_invalidate_w)
+    ,.mem_tcm_flush_o(dport_tcm_flush_w)
+    ,.mem_ext_addr_o(dport_axi_addr_w)
+    ,.mem_ext_data_wr_o(dport_axi_data_wr_w)
+    ,.mem_ext_rd_o(dport_axi_rd_w)
+    ,.mem_ext_wr_o(dport_axi_wr_w)
+    ,.mem_ext_cacheable_o(dport_axi_cacheable_w)
+    ,.mem_ext_req_tag_o(dport_axi_req_tag_w)
+    ,.mem_ext_invalidate_o(dport_axi_invalidate_w)
+    ,.mem_ext_flush_o(dport_axi_flush_w)
+);
+
+
+tcm_mem
+u_tcm
+(
+    // Inputs
+     .clk_i(clk_i)
+    ,.rst_i(rst_i)
+    ,.mem_i_rd_i(ifetch_rd_w)
+    ,.mem_i_flush_i(ifetch_flush_w)
+    ,.mem_i_invalidate_i(ifetch_invalidate_w)
+    ,.mem_i_pc_i(ifetch_pc_w)
+    ,.mem_d_addr_i(dport_tcm_addr_w)
+    ,.mem_d_data_wr_i(dport_tcm_data_wr_w)
+    ,.mem_d_rd_i(dport_tcm_rd_w)
+    ,.mem_d_wr_i(dport_tcm_wr_w)
+    ,.mem_d_cacheable_i(dport_tcm_cacheable_w)
+    ,.mem_d_req_tag_i(dport_tcm_req_tag_w)
+    ,.mem_d_invalidate_i(dport_tcm_invalidate_w)
+    ,.mem_d_flush_i(dport_tcm_flush_w)
+    ,.axi_awvalid_i(axi_t_awvalid_i)
+    ,.axi_awaddr_i(axi_t_awaddr_i)
+    ,.axi_awid_i(axi_t_awid_i)
+    ,.axi_awlen_i(axi_t_awlen_i)
+    ,.axi_awburst_i(axi_t_awburst_i)
+    ,.axi_wvalid_i(axi_t_wvalid_i)
+    ,.axi_wdata_i(axi_t_wdata_i)
+    ,.axi_wstrb_i(axi_t_wstrb_i)
+    ,.axi_wlast_i(axi_t_wlast_i)
+    ,.axi_bready_i(axi_t_bready_i)
+    ,.axi_arvalid_i(axi_t_arvalid_i)
+    ,.axi_araddr_i(axi_t_araddr_i)
+    ,.axi_arid_i(axi_t_arid_i)
+    ,.axi_arlen_i(axi_t_arlen_i)
+    ,.axi_arburst_i(axi_t_arburst_i)
+    ,.axi_rready_i(axi_t_rready_i)
+
+    // Outputs
+    ,.mem_i_accept_o(ifetch_accept_w)
+    ,.mem_i_valid_o(ifetch_valid_w)
+    ,.mem_i_error_o(ifetch_error_w)
+    ,.mem_i_inst_o(ifetch_inst_w)
+    ,.mem_d_data_rd_o(dport_tcm_data_rd_w)
+    ,.mem_d_accept_o(dport_tcm_accept_w)
+    ,.mem_d_ack_o(dport_tcm_ack_w)
+    ,.mem_d_error_o(dport_tcm_error_w)
+    ,.mem_d_resp_tag_o(dport_tcm_resp_tag_w)
+    ,.axi_awready_o(axi_t_awready_o)
+    ,.axi_wready_o(axi_t_wready_o)
+    ,.axi_bvalid_o(axi_t_bvalid_o)
+    ,.axi_bresp_o(axi_t_bresp_o)
+    ,.axi_bid_o(axi_t_bid_o)
+    ,.axi_arready_o(axi_t_arready_o)
+    ,.axi_rvalid_o(axi_t_rvalid_o)
+    ,.axi_rdata_o(axi_t_rdata_o)
+    ,.axi_rresp_o(axi_t_rresp_o)
+    ,.axi_rid_o(axi_t_rid_o)
+    ,.axi_rlast_o(axi_t_rlast_o)
+);
+
+
+dport_axi
+u_axi
+(
+    // Inputs
+     .clk_i(clk_i)
+    ,.rst_i(rst_i)
+    ,.mem_addr_i(dport_axi_addr_w)
+    ,.mem_data_wr_i(dport_axi_data_wr_w)
+    ,.mem_rd_i(dport_axi_rd_w)
+    ,.mem_wr_i(dport_axi_wr_w)
+    ,.mem_cacheable_i(dport_axi_cacheable_w)
+    ,.mem_req_tag_i(dport_axi_req_tag_w)
+    ,.mem_invalidate_i(dport_axi_invalidate_w)
+    ,.mem_flush_i(dport_axi_flush_w)
+    ,.axi_awready_i(axi_i_awready_i)
+    ,.axi_wready_i(axi_i_wready_i)
+    ,.axi_bvalid_i(axi_i_bvalid_i)
+    ,.axi_bresp_i(axi_i_bresp_i)
+    ,.axi_bid_i(axi_i_bid_i)
+    ,.axi_arready_i(axi_i_arready_i)
+    ,.axi_rvalid_i(axi_i_rvalid_i)
+    ,.axi_rdata_i(axi_i_rdata_i)
+    ,.axi_rresp_i(axi_i_rresp_i)
+    ,.axi_rid_i(axi_i_rid_i)
+    ,.axi_rlast_i(axi_i_rlast_i)
+
+    // Outputs
+    ,.mem_data_rd_o(dport_axi_data_rd_w)
+    ,.mem_accept_o(dport_axi_accept_w)
+    ,.mem_ack_o(dport_axi_ack_w)
+    ,.mem_error_o(dport_axi_error_w)
+    ,.mem_resp_tag_o(dport_axi_resp_tag_w)
+    ,.axi_awvalid_o(axi_i_awvalid_o)
+    ,.axi_awaddr_o(axi_i_awaddr_o)
+    ,.axi_awid_o(axi_i_awid_o)
+    ,.axi_awlen_o(axi_i_awlen_o)
+    ,.axi_awburst_o(axi_i_awburst_o)
+    ,.axi_wvalid_o(axi_i_wvalid_o)
+    ,.axi_wdata_o(axi_i_wdata_o)
+    ,.axi_wstrb_o(axi_i_wstrb_o)
+    ,.axi_wlast_o(axi_i_wlast_o)
+    ,.axi_bready_o(axi_i_bready_o)
+    ,.axi_arvalid_o(axi_i_arvalid_o)
+    ,.axi_araddr_o(axi_i_araddr_o)
+    ,.axi_arid_o(axi_i_arid_o)
+    ,.axi_arlen_o(axi_i_arlen_o)
+    ,.axi_arburst_o(axi_i_arburst_o)
+    ,.axi_rready_o(axi_i_rready_o)
+);
+
+
+
+endmodule
